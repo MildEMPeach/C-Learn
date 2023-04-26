8a39061d2c561f2e6f7c62f37762548feee37ad5
MildEMPeach <2268812626@qq.com>
2023-04-26 21:39:31 +0800

    Changes to be committed: 	modified:   YinYong.cpp

--- YinYong.cpp
+++ YinYong.cpp
@@ -1,14 +1,25 @@
 //引用
 #include <iostream>
 using namespace std;
+//修改a，b的值，本来要用指针，传递a，b的地址，现在可以用引用。
 
+
+
+void swap(int &a,int &b){
+    int temp=a;
+    a=b;
+    b=temp;
+}
 int main(void){
     int a=10;
+    int c=8;
     int &b =a;
     cout<<a<<" "<<b<<endl;
     a=6;
     cout<<a<<" "<<b<<endl;
     b=5;
     cout<<a<<" "<<b<<endl;
+    swap(a,c);
+    cout<<a<<" "<<c<<endl;
     return 0;
 }
